a56926c1d5d2f2a0f4efda2d63a5e951d533dde9
Haseong Kim <ethankim0421@gmail.com>
2022-10-19 17:38:28 +0100

    Added a function to create a new dom inside of DOM and working on Caret

--- packages/finer/ts/packages/dom/DOMUtils.ts
+++ packages/finer/ts/packages/dom/DOMUtils.ts
@@ -1,9 +1,21 @@
 import { Utils, Type } from 'dynafer/utils';
 import Options, { EModeEditor } from '../../Options';
 
+const NativeEvents: string[] = [
+	'abort animationcancel animationend animationiteration animationstart auxclick beforeinput blur cancel canplay canplaythrough change click close',
+	'compositionend compositionstart compositionupdate contextmenu cuechange dblclick drag dragend dragenter dragleave dragover dragstart drop durationchange',
+	'emptied ended error focus focusin focusout formdata gotpointercapture input invalid keydown keypress keyup load loadeddata loadedmetadata',
+	'loadstart lostpointercapture mousedown mouseenter mouseleave mousemove mouseout mouseover mouseup pause play playing pointercancel pointerdown',
+	'pointerenter pointerleave pointermove pointerout pointerover pointerup progress ratechange reset resize scroll securitypolicyviolation seeked seeking',
+	'select selectionchange selectstart slotchange stalled submit suspend timeupdate toggle touchcancel touchend touchmove touchstart transitioncancel',
+	'transitionend transitionrun transitionstart volumechange waiting webkitanimationend webkitanimationiteration webkitanimationstart webkittransitionend wheel'
+].join(' ').split(' ');
+
 export interface IDOMUtils {
+	NativeEvents: string[],
 	CreateUEID: (id?: string, addNumber?: boolean) => string,
 	GetModeTag: (mode: EModeEditor) => string,
+	GetLineNumber: (selector: Element) => number,
 }
 
 const DOMUtils = (): IDOMUtils => {
@@ -22,9 +34,18 @@ const DOMUtils = (): IDOMUtils => {
 		}
 	};
 
+	const GetLineNumber = (selector: Element): number => {
+		const parent: ParentNode | null = selector.parentNode;
+		if (!Type.IsElement(parent)) return 0;
+
+		return Array.from(parent.children).indexOf(parent);
+	};
+
 	return {
+		NativeEvents,
 		CreateUEID,
 		GetModeTag,
+		GetLineNumber,
 	};
 };
 
