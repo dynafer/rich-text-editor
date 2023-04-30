f1a736cb8ccd3957a103e612efe8d2700d427878
Haseong Kim <ethankim0421@gmail.com>
2023-04-30 18:30:39 +0100

    Made testing for @dynafer/interlocaliser/LanguageStorage more readable.

--- packages/dynafer/interlocaliser/test/LanguageStorage.test.ts
+++ packages/dynafer/interlocaliser/test/LanguageStorage.test.ts
@@ -5,27 +5,25 @@ const TestLanguageStorage = () =>
 		// Clear the Languages before each test
 		beforeEach(() => Object.keys(LanguageStorage.Languages).forEach((key) => delete LanguageStorage.Languages?.[key]));
 
-		test('Add() should add a key-value pair to the language map', () => {
+		it('should add a key-value pair to the language map with Add()', () => {
 			LanguageStorage.Add('en', 'greeting', 'Hello');
 			expect(LanguageStorage.Languages).toEqual({ en: { greeting: 'Hello' } });
 		});
 
-		test('AddMap() should add multiple key-value pairs to the language map', () => {
+		it('should add multiple key-value pairs to the language map with AddMap()', () => {
 			const map = { greeting: 'Hello', farewell: 'Goodbye' };
 			LanguageStorage.AddMap('en', map);
 			expect(LanguageStorage.Languages).toEqual({ en: map });
 		});
 
-		test('Get() should return the value for a given key in the language map', () => {
+		it('should return the value for a given key in the language map with Get()', () => {
 			LanguageStorage.Add('en', 'greeting', 'Hello');
 			expect(LanguageStorage.Get('en', 'greeting')).toBe('Hello');
 		});
 
-		test('Get() should return null if the key does not exist in the language map', () => {
-			expect(LanguageStorage.Get('en', 'greeting')).toBeNull();
-		});
+		it('should return null if the key does not exist in the language map with Get()', () => expect(LanguageStorage.Get('en', 'greeting')).toBeNull());
 
-		test('Remove() should remove a language from the language map', () => {
+		it('should remove a language from the language map with Remove()', () => {
 			LanguageStorage.Add('en', 'greeting', 'Hello');
 			LanguageStorage.Remove('en');
 			expect(LanguageStorage.Languages).toEqual({});
